e9b22275f1f6d9990e86d8ffd11d2b0fbe208056
meet-radadiya13 <meet.r@simformsolutions.com>
2023-03-23 10:30:14 +0530

    Added documentation in code -- practical2.py

--- practical2.py
+++ practical2.py
@@ -5,11 +5,16 @@
 
 def generate_braces(n, memo={}):
     """
-    The code generates all possible combinations of well-formed parentheses given an input n (number of pairs of
-    parentheses). It uses a recursive approach where it generates all combinations of well-formed parentheses for
-    values less than n and then combines them in all possible ways to generate combinations for n. For further
-    optimization code uses memoization, which stores the results for each value of n in a dictionary, to reduce the
-    number of recursive calls and improve the overall runtime.
+    Generates all possible valid combinations of n pairs of parentheses.
+
+    Args:
+    n (int): The number of pairs of parentheses to generate.
+
+    Returns:
+    list: A list of strings representing all valid combinations of n pairs of parentheses.
+
+    Raises:
+    None
     """
     if n in memo:
         return memo[n]
