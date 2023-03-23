7088a84b6d82a0c734023bf3f09417f592353ed7
meet-radadiya13 <meet.r@simformsolutions.com>
2023-03-23 10:19:01 +0530

    Added memorization for optimization

--- practical2.py
+++ practical2.py
@@ -3,15 +3,25 @@
 """
 
 
-def generate_braces(n):
+def generate_braces(n, memo={}):
+    """
+    The code generates all possible combinations of well-formed parentheses given an input n (number of pairs of
+    parentheses). It uses a recursive approach where it generates all combinations of well-formed parentheses for
+    values less than n and then combines them in all possible ways to generate combinations for n. For further
+    optimization code uses memoization, which stores the results for each value of n in a dictionary, to reduce the
+    number of recursive calls and improve the overall runtime.
+    """
+    if n in memo:
+        return memo[n]
     output = []
     if n == 0:
         output.append('')
     else:
         for i in range(n):
-            for left in generate_braces(i):
-                for right in generate_braces(n - i - 1):
+            for left in generate_braces(i, memo):
+                for right in generate_braces(n - i - 1, memo):
                     output.append('({}){}'.format(left, right))
+    memo[n] = output
     return output
 
 
